991ff9b1987de59915a78cd3ee49c5de4d819f24
Rishabbh <69572197+Rishabbh-Sahu@users.noreply.github.com>
2022-03-22 23:55:58 +0000

    Update preprocessing.py

--- text_preprocessing/preprocessing.py
+++ text_preprocessing/preprocessing.py
@@ -12,11 +12,11 @@ def remove_next_line(txt_arr):
     return [sub.replace('\n', '') for sub in txt_arr]
 
 def remove_punctuations(txt_arr):
-    '''
+    """
     data cleansing in order to remove various punctuations exits in the file
     txt_arr: list [] - [[sequence of text1],[sequence of text2]...]
     return: cleaned text without specified punctuations
-    '''
+    """
     return txt_arr
 
 def text_to_word_list(sentences):
